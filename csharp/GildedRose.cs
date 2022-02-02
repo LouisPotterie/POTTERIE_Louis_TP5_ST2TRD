8fabdf5d422efabc0bf05c5ca051cfed45267451
louis <louis.potterie@esigetel.net>
2022-02-02 23:44:26 +0100

    update 2

--- csharp/GildedRose.cs
+++ csharp/GildedRose.cs
@@ -20,62 +20,66 @@ namespace csharp
             Items[i].Quality = Items[i].Quality + 1;
         }
         
+        private void SellInDecrease(int i)
+        {
+            Items[i].SellIn = Items[i].SellIn - 1;
+        }
+        
 
         public void UpdateQuality()
         {
             for (var i = 0; i < Items.Count; i++)
             {
-                if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
                 {
-                    if (Items[i].Quality > 0)
+                    if (Items[i].Name is "Aged Brie" or "Backstage passes to a TAFKAL80ETC concert") 
                     {
-                        if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
+                        if (Items[i].Quality < 50)
+                        {
+                            IncreaseQuality(i);
+
+                            if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
+                            { 
+                                if (Items[i].SellIn < 11) 
+                                { 
+                                    if (Items[i].Quality < 50) 
+                                    { 
+                                        IncreaseQuality(i); 
+                                    } 
+                                }
+                                if (Items[i].SellIn < 6) 
+                                { 
+                                    if (Items[i].Quality < 50) 
+                                    { 
+                                        IncreaseQuality(i); 
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    else
+                    {
+                        if (Items[i].Quality > 0)
                         {
                             DecreaseQuality(i);
                         }
+                        
                     }
-                }
-                else
-                {
-                    if (Items[i].Quality < 50)
+                    
+                    SellInDecrease(i);
+                    
+                    if (Items[i].SellIn < 0)
                     {
-                        IncreaseQuality(i);
-
-                        if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
+                        if (Items[i].Name != "Aged Brie")
                         {
-                            if (Items[i].SellIn < 11)
+                            if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
                             {
-                                if (Items[i].Quality < 50)
-                                {
-                                    IncreaseQuality(i);
-                                }
+                                
+                                Items[i].Quality = Items[i].Quality - Items[i].Quality;
                             }
-
-                            if (Items[i].SellIn < 6)
+                            else
                             {
-                                if (Items[i].Quality < 50)
-                                {
-                                    IncreaseQuality(i);
-                                }
-                            }
-                        }
-                    }
-                }
-
-                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                {
-                    Items[i].SellIn = Items[i].SellIn - 1;
-                }
-
-                if (Items[i].SellIn < 0)
-                {
-                    if (Items[i].Name != "Aged Brie")
-                    {
-                        if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            if (Items[i].Quality > 0)
-                            {
-                                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
+                                if (Items[i].Quality > 0)
                                 {
                                     DecreaseQuality(i);
                                 }
@@ -83,17 +87,14 @@ namespace csharp
                         }
                         else
                         {
-                            Items[i].Quality = Items[i].Quality - Items[i].Quality;
+                            if (Items[i].Quality < 50)
+                            {
+                                IncreaseQuality(i);
+                            }
                         }
-                    }
-                    else
-                    {
-                        if (Items[i].Quality < 50)
-                        {
-                            IncreaseQuality(i);
-                        }
-                    }
+                    } 
                 }
+                
             }
         }
 
---
+++ csharp/Sulfuras.cs
@@ -0,0 +1,15 @@
+﻿using System.Net.Http.Headers;
+
+namespace csharp
+{
+    public class Sulfuras : Item
+    {
+        public string Name { get; set; } = "Sulfuras, Hand of Ragnaros";
+        public int Quality { get; set; } = 80;
+        
+        public override string ToString()
+        {
+            return this.Name + ", " + this.SellIn + ", " + this.Quality;
+        }  
+    }
+}
