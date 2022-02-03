b6b7f9f59fe4b2917eb16e9f801a98cf58e4788a
louis <louis.potterie@esigetel.net>
2022-02-03 17:43:06 +0100

    Question 3 done

--- csharp/GildedRose.cs
+++ csharp/GildedRose.cs
@@ -10,121 +10,13 @@ namespace csharp
             this.Items = Items;
         }
         
-        private void DecreaseQuality(Item item)
-        {
-            item.Quality -= 1;
-        }
-        
-        private void IncreaseQuality(Item item)
-        {
-            item.Quality += 1;
-        }
-        
-        private void SellInDecrease(Item item)
-        {
-            item.SellIn -= 1;
-        }
-        
-        private bool IsBackstage(Item item)
-        {
-            return item.Name == "Backstage passes to a TAFKAL80ETC concert";
-        }
-        
-        private bool IsNotSulfuras(Item item)
-        {
-            return item.Name != "Sulfuras, Hand of Ragnaros";
-        }
-        
-        private void ResetQualityToZero(Item item)
-        { 
-            item.Quality -= item.Quality;
-        }
-        
-        private bool isAgedBrie(Item item)
-        {
-            return item.Name == "Aged Brie";
-        }
-        
-
         public void UpdateQuality()
         {
             foreach (var item in Items)
             {
-                if (IsNotSulfuras(item))
-                {
-                    if (item.Name is "Aged Brie" or "Backstage passes to a TAFKAL80ETC concert") 
-                    {
-                        if (item.Quality < 50)
-                        {
-                            IncreaseQuality(item);
-
-                            if (IsBackstage(item))
-                            { 
-                                if (item.SellIn < 11) 
-                                { 
-                                    if (item.Quality < 50) 
-                                    { 
-                                        IncreaseQuality(item); 
-                                    } 
-                                }
-                                if (item.SellIn < 6) 
-                                { 
-                                    if (item.Quality < 50) 
-                                    { 
-                                        IncreaseQuality(item); 
-                                    }
-                                }
-                            }
-                        }
-                    }
-                    else
-                    {
-                        if (item.Quality > 0)
-                        {
-                            DecreaseQuality(item);
-                        }
-                        
-                    }
-                    
-                    SellInDecrease(item);
-                    
-                    if (item.SellIn < 0)
-                    {
-                        
-                        switch (isAgedBrie(item))
-                        {
-                            
-                            case true :
-                                if (item.Quality < 50)
-                                {
-                                    IncreaseQuality(item);
-                                }
-                                break;
-                            
-                            case false :
-                                if (IsBackstage(item))
-                                {
-                                    ResetQualityToZero(item);
-                                }
-                                else
-                                {
-                                    if (item.Quality > 0)
-                                    {
-                                        DecreaseQuality(item);
-                                    }
-                                }
-                                break;
-
-
-                        }
-                        
-                    } 
-                }
-                
+                new ItemUpdate(item).UpdateItem();
             }
-            
-            
-            
+
         }
 
         
---
+++ csharp/ItemUpdate.cs
@@ -0,0 +1,125 @@
+﻿namespace csharp
+{
+    public class ItemUpdate
+    {
+        private Item item;
+
+        public ItemUpdate(Item item)
+        {
+            this.item = item;
+        }
+        
+        private void DecreaseQuality(Item item)
+        {
+            item.Quality -= 1;
+        }
+        
+        private void IncreaseQuality(Item item)
+        {
+            item.Quality += 1;
+        }
+        
+        private void SellInDecrease(Item item)
+        {
+            item.SellIn -= 1;
+        }
+        
+        private bool IsBackstage(Item item)
+        {
+            return item.Name == "Backstage passes to a TAFKAL80ETC concert";
+        }
+        
+        private bool IsNotSulfuras(Item item)
+        {
+            return item.Name != "Sulfuras, Hand of Ragnaros";
+        }
+        
+        private void ResetQualityToZero(Item item)
+        { 
+            item.Quality -= item.Quality;
+        }
+        
+        private bool isAgedBrie(Item item)
+        {
+            return item.Name == "Aged Brie";
+        }
+        
+        public void UpdateItem()
+        {
+            
+                if (IsNotSulfuras(item))
+                {
+                    if (item.Name is "Aged Brie" or "Backstage passes to a TAFKAL80ETC concert") 
+                    {
+                        if (item.Quality < 50)
+                        {
+                            IncreaseQuality(item);
+
+                            if (IsBackstage(item))
+                            { 
+                                if (item.SellIn < 11) 
+                                { 
+                                    if (item.Quality < 50) 
+                                    { 
+                                        IncreaseQuality(item); 
+                                    } 
+                                }
+                                if (item.SellIn < 6) 
+                                { 
+                                    if (item.Quality < 50) 
+                                    { 
+                                        IncreaseQuality(item); 
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    else
+                    {
+                        if (item.Quality > 0)
+                        {
+                            DecreaseQuality(item);
+                        }
+                        
+                    }
+                    
+                    SellInDecrease(item);
+                    
+                    if (item.SellIn < 0)
+                    {
+                        
+                        switch (isAgedBrie(item))
+                        {
+                            
+                            case true :
+                                if (item.Quality < 50)
+                                {
+                                    IncreaseQuality(item);
+                                }
+                                break;
+                            
+                            case false :
+                                if (IsBackstage(item))
+                                {
+                                    ResetQualityToZero(item);
+                                }
+                                else
+                                {
+                                    if (item.Quality > 0)
+                                    {
+                                        DecreaseQuality(item);
+                                    }
+                                }
+                                break;
+
+
+                        }
+                        
+                    } 
+                }
+                
+        }
+        
+        
+    }
+}
